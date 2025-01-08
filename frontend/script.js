0efd4fa5d66a90bb3e466e60550ab1308f04ad14
SawTin <166284006+SawTin@users.noreply.github.com>
2025-01-08 22:53:22 +0800

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('fetchDataBtn').addEventListener('click', () => {
-    fetch('/api/data')
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('dataDisplay').innerText = data.message;
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  });
